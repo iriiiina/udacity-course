5ee6902c9e104428b23cd162b16702a153e3cb47
iriiiina <irina.ivanova@protonmail.com>
2022-02-22 19:01:02 +0200

    2 Code a Custom Category Widget

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -10,13 +10,27 @@ import 'package:flutter/material.dart';
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
+
+const _height = 100.0;
+const _borderRadius = _height / 2;
+
 class Category extends StatelessWidget {
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category({Key? key}) : super(key: key);
+  // DONE: You'll need the name, color, and iconLocation from main.dart
+
+  const Category({
+    Key? key,
+    required this.name,
+    required this.color,
+    required this.icon,
+  }) : super(key: key);
+
+  final String name;
+  final Color color;
+  final IconData icon;
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -27,7 +41,41 @@ class Category extends StatelessWidget {
   // Theme ancestor in the tree. Below, we obtain the display1 text theme.
   // See https://api.flutter.dev/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    // DONE: Build the custom widget here, referring to the Specs.
+    return InkWell(
+      onTap: () {
+        print('I was tapped!');
+      },
+      splashColor: color,
+      highlightColor: color,
+      hoverColor: Colors.transparent,
+      focusColor: Colors.transparent,
+      borderRadius: BorderRadius.circular(_borderRadius),
+      child: Container(
+        height: _height,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          border: Border.all(width: 1.0, color: Colors.black),
+          borderRadius: BorderRadius.circular(_borderRadius),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Icon(
+                icon,
+                size: 60.0,
+              ),
+            ),
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
